7a320e02c80509d0b6c16b0f9d6a7c2b3732f76d
tunaizi <tunaizi@users.noreply.github.com>
2025-01-06 06:42:21 +0000

    deploy: 47bae354cba34effcaeabf1a7a38d223b46f2c0b

---
+++ assets/start-docs_B-专题_bind_call_apply.md.c27db423.lean.js
@@ -0,0 +1,111 @@
+import{_ as n,r as a,i as l,a as p}from"./chunks/framework.bb8f9838.js";const F=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[{"level":2,"title":"bind & call & apply","slug":"bind-call-apply","link":"#bind-call-apply","children":[]}],"relativePath":"start-docs/B-专题/bind&call&apply.md","filePath":"start-docs/B-专题/bind&call&apply.md"}'),o={name:"start-docs/B-专题/bind&call&apply.md"};function e(c,s,t,r,E,i){return a(),l("div",null,s[0]||(s[0]=[p(`<h2 id="bind-call-apply" tabindex="-1">bind &amp; call &amp; apply <a class="header-anchor" href="#bind-call-apply" aria-label="Permalink to &quot;bind &amp; call &amp; apply&quot;">​</a></h2><ul><li>call 和 apply,是为了动态改变 this 而出现。当一个 object 没有某个方法，但是其他对象有，就可以借助 call 和 apply 来使用其他对象的方法。</li></ul><div class="language-js"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki github-dark"><code><span class="line"><span style="color:#6A737D;">// document.getElementsByTagName选择的dom 节点是一种类似array的array。它不能应用Array下的push,pop等方法</span></span>
+<span class="line"><span style="color:#F97583;">var</span><span style="color:#E1E4E8;"> domNodes </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#79B8FF;">Array</span><span style="color:#E1E4E8;">.</span><span style="color:#79B8FF;">prototype</span><span style="color:#E1E4E8;">.slice.</span><span style="color:#B392F0;">call</span><span style="color:#E1E4E8;">(document.</span><span style="color:#B392F0;">getElementByTagName</span><span style="color:#E1E4E8;">(</span><span style="color:#9ECBFF;">&quot;*&quot;</span><span style="color:#E1E4E8;">));</span></span></code></pre></div><div class="language-js"><button title="Copy Code" class="copy"></button><span class="lang">js</span><pre class="shiki github-dark"><code><span class="line"><span style="color:#F97583;">function</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">Cat</span><span style="color:#E1E4E8;">(</span><span style="color:#FFAB70;">name</span><span style="color:#E1E4E8;">) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#79B8FF;">this</span><span style="color:#E1E4E8;">.name </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> name</span></span>
+<span class="line"><span style="color:#E1E4E8;">    </span><span style="color:#79B8FF;">this</span><span style="color:#E1E4E8;">.</span><span style="color:#B392F0;">showName</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">function</span><span style="color:#E1E4E8;">(</span><span style="color:#FFAB70;">args</span><span style="color:#E1E4E8;">) {</span></span>
+<span class="line"><span style="color:#E1E4E8;">      console.</span><span style="color:#B392F0;">log</span><span style="color:#E1E4E8;">(</span><span style="color:#79B8FF;">this</span><span style="color:#E1E4E8;">.name,args)</span></span>
+<span class="line"><span style="color:#E1E4E8;">    }</span></span>
+<span class="line"><span style="color:#E1E4E8;">  }</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">let</span><span style="color:#E1E4E8;"> cat </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> </span><span style="color:#F97583;">new</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">Cat</span><span style="color:#E1E4E8;">(</span><span style="color:#9ECBFF;">&#39;aa&#39;</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#E1E4E8;">  cat.</span><span style="color:#B392F0;">showName</span><span style="color:#E1E4E8;">(</span><span style="color:#9ECBFF;">&#39;1&#39;</span><span style="color:#E1E4E8;">) </span><span style="color:#6A737D;">// aa 1</span></span>
+<span class="line"><span style="color:#E1E4E8;">  </span><span style="color:#F97583;">let</span><span style="color:#E1E4E8;"> dog </span><span style="color:#F97583;">=</span><span style="color:#E1E4E8;"> {name: </span><span style="color:#9ECBFF;">&#39;bb&#39;</span><span style="color:#E1E4E8;">}</span></span>
+<span class="line"><span style="color:#E1E4E8;">  cat.showName.</span><span style="color:#B392F0;">call</span><span style="color:#E1E4E8;">(dog, </span><span style="color:#9ECBFF;">&#39;1&#39;</span><span style="color:#E1E4E8;">)  </span><span style="color:#6A737D;">// bb 1</span></span>
+<span class="line"><span style="color:#E1E4E8;">  cat.showName.</span><span style="color:#B392F0;">apply</span><span style="color:#E1E4E8;">(dog, [</span><span style="color:#9ECBFF;">&#39;2&#39;</span><span style="color:#E1E4E8;">]) </span><span style="color:#6A737D;">// bb 2</span></span>
+<span class="line"><span style="color:#9ECBFF;">\`\`\`</span></span>
+<span class="line"><span style="color:#9ECBFF;">- call与apply,接受参数不同。**call将多个参数分开接收**，apply将参数合并为数组接收。</span></span>
+<span class="line"><span style="color:#9ECBFF;">- bind方法也可以改变this指针。 与call和apply的区别在于不会立即执行。bind方法会预先将参数传递给方法。bind() 方法创建一个新的函数，在 bind() 被调用时，这个新函数的 this 被指定为 bind() 的第一个参数，而其余参数将作为新函数的参数，供调用时使用。</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">## call实现</span></span>
+<span class="line"><span style="color:#9ECBFF;">\`\`\`\`js</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">Function.prototype.myCall = function(context) {</span></span>
+<span class="line"><span style="color:#9ECBFF;">if(typeof this !== &#39;function&#39;) {</span></span>
+<span class="line"><span style="color:#9ECBFF;">throw new TypeError(&#39;not function&#39;)</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"><span style="color:#9ECBFF;">//获取当前上下文</span></span>
+<span class="line"><span style="color:#9ECBFF;">context = context || window</span></span>
+<span class="line"><span style="color:#9ECBFF;">//将指针赋值给当前上下文中的函数</span></span>
+<span class="line"><span style="color:#9ECBFF;">context.fn = this</span></span>
+<span class="line"><span style="color:#9ECBFF;">//获取参数，过滤掉第一个参数</span></span>
+<span class="line"><span style="color:#9ECBFF;">let arg = [...arguments].slice(1)</span></span>
+<span class="line"><span style="color:#9ECBFF;">//执行函数</span></span>
+<span class="line"><span style="color:#9ECBFF;">let result = context.fn(...arg)</span></span>
+<span class="line"><span style="color:#9ECBFF;">//删除绑定</span></span>
+<span class="line"><span style="color:#9ECBFF;">delete context.fn</span></span>
+<span class="line"><span style="color:#9ECBFF;">//返回结果</span></span>
+<span class="line"><span style="color:#9ECBFF;">return result</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">function foo() {</span></span>
+<span class="line"><span style="color:#9ECBFF;">this.name = &#39;foo&#39;</span></span>
+<span class="line"><span style="color:#9ECBFF;">this.showName = function() {</span></span>
+<span class="line"><span style="color:#9ECBFF;">console.log(this.name, arguments)</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"><span style="color:#9ECBFF;">};</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">(new foo(&#39;foo&#39;)).showName.call({name: &#39;fooooo&#39;}, &#39;aaa&#39;, &#39;bbb&#39;);</span></span>
+<span class="line"><span style="color:#9ECBFF;">(new foo(&#39;foo&#39;)).showName.myCall({name: &#39;fooooo&#39;}, &#39;aaa&#39;, &#39;bbb&#39;);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">\`\`\`</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">## apply实现</span></span>
+<span class="line"><span style="color:#9ECBFF;">\`\`\`</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">Function.prototype.myApply = function(context) {</span></span>
+<span class="line"><span style="color:#9ECBFF;">if(typeof this !== &#39;function&#39;) {</span></span>
+<span class="line"><span style="color:#9ECBFF;">throw new TypeError(&#39;is not function&#39;)</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"><span style="color:#9ECBFF;">context = context || window</span></span>
+<span class="line"><span style="color:#9ECBFF;">context.fn = this</span></span>
+<span class="line"><span style="color:#9ECBFF;">let result</span></span>
+<span class="line"><span style="color:#9ECBFF;">if(arguments[1]) {</span></span>
+<span class="line"><span style="color:#9ECBFF;">context.fn(...arguments[1])</span></span>
+<span class="line"><span style="color:#9ECBFF;">} else {</span></span>
+<span class="line"><span style="color:#9ECBFF;">context.fn()</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"><span style="color:#9ECBFF;">delete context.fn</span></span>
+<span class="line"><span style="color:#9ECBFF;">return result</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">function foo() {</span></span>
+<span class="line"><span style="color:#9ECBFF;">this.name = &#39;foo&#39;</span></span>
+<span class="line"><span style="color:#9ECBFF;">this.showName = function() {</span></span>
+<span class="line"><span style="color:#9ECBFF;">console.log(this.name, arguments)</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"><span style="color:#9ECBFF;">};</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">(new foo(&#39;foo&#39;)).showName.apply({name: &#39;fooooo&#39;}, [&#39;aaa&#39;, &#39;bbb&#39;]);</span></span>
+<span class="line"><span style="color:#9ECBFF;">(new foo(&#39;foo&#39;)).showName.myApply({name: &#39;fooooo&#39;}, [&#39;aaa&#39;, &#39;bbb&#39;]);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">\`\`\`</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">## bind实现</span></span>
+<span class="line"><span style="color:#9ECBFF;">\`\`\`</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">//使用 apply 实现 bind</span></span>
+<span class="line"><span style="color:#9ECBFF;">Function.prototype.myBind = function(context) {</span></span>
+<span class="line"><span style="color:#9ECBFF;">if(typeof this !== &#39;function&#39;) {</span></span>
+<span class="line"><span style="color:#9ECBFF;">throw new TypeError(&#39;error&#39;)</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"><span style="color:#9ECBFF;">let self = this</span></span>
+<span class="line"><span style="color:#9ECBFF;">return function() {</span></span>
+<span class="line"><span style="color:#9ECBFF;">return self.apply(context, arguments)</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">function Foo() {</span></span>
+<span class="line"><span style="color:#9ECBFF;">this.name = &#39;foo&#39;</span></span>
+<span class="line"><span style="color:#9ECBFF;">this.showName = function() {</span></span>
+<span class="line"><span style="color:#9ECBFF;">console.log(this.name, arguments)</span></span>
+<span class="line"><span style="color:#9ECBFF;">}</span></span>
+<span class="line"><span style="color:#9ECBFF;">};</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">let fn = (new Foo(&#39;foo&#39;)).showName.bind({name: &#39;foooooo&#39;});</span></span>
+<span class="line"><span style="color:#9ECBFF;">fn(&#39;aaa&#39;,&#39;bbb&#39;);</span></span>
+<span class="line"><span style="color:#9ECBFF;">let fn1 = (new Foo(&#39;foo&#39;)).showName.myBind({name: &#39;foooooo&#39;});</span></span>
+<span class="line"><span style="color:#9ECBFF;">fn1(&#39;aaa&#39;,&#39;bbb&#39;);</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#9ECBFF;">\`\`\`</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">### </span><span style="color:#B392F0;">箭头函数中任然可以使用call</span><span style="color:#E1E4E8;">()、</span><span style="color:#B392F0;">apply</span><span style="color:#E1E4E8;">()、</span><span style="color:#B392F0;">bind</span><span style="color:#E1E4E8;">()方法，但是this值不会被影响。</span></span>
+<span class="line"><span style="color:#9ECBFF;">\`\`\`</span></span></code></pre></div>`,4)]))}const B=n(o,[["render",e]]);export{F as __pageData,B as default};
---
+++ assets/start-docs_B-专题_柯里化.md.77bad950.lean.js
@@ -0,0 +1,39 @@
+import{_ as n,r as e,i as a,a as l}from"./chunks/framework.bb8f9838.js";const u=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[{"level":2,"title":"柯里化 currying","slug":"柯里化-currying","link":"#柯里化-currying","children":[]}],"relativePath":"start-docs/B-专题/柯里化.md","filePath":"start-docs/B-专题/柯里化.md"}'),p={name:"start-docs/B-专题/柯里化.md"};function t(c,s,o,r,i,y){return e(),a("div",null,s[0]||(s[0]=[l(`<h2 id="柯里化-currying" tabindex="-1">柯里化 currying <a class="header-anchor" href="#柯里化-currying" aria-label="Permalink to &quot;柯里化 currying&quot;">​</a></h2><ul><li><p>在数学和计算机科学中，柯里化是一种将使用多个参数的一个函数转换成一系列使用一个参数的函数的技术。</p><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark"><code><span class="line"><span style="color:#e1e4e8;">function add(a, b) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">    return a + b</span></span>
+<span class="line"><span style="color:#e1e4e8;">  }</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">  add(1,2) //3</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">  var addCurry = curry(add)</span></span>
+<span class="line"><span style="color:#e1e4e8;">  addCurry(1)(1) //3</span></span></code></pre></div><div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark"><code><span class="line"><span style="color:#e1e4e8;">function sub_curry(fn) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">    var args = [].slice.call(arguments, 1);</span></span>
+<span class="line"><span style="color:#e1e4e8;">    return function() {</span></span>
+<span class="line"><span style="color:#e1e4e8;">      var newArgs = args.concat([].slice.call(arguments));</span></span>
+<span class="line"><span style="color:#e1e4e8;">      return fn.apply(this, newArgs);</span></span>
+<span class="line"><span style="color:#e1e4e8;">    }</span></span>
+<span class="line"><span style="color:#e1e4e8;">  }</span></span>
+<span class="line"><span style="color:#e1e4e8;"></span></span>
+<span class="line"><span style="color:#e1e4e8;">  function curry(fn, length) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">    length = length || fn.length;</span></span>
+<span class="line"><span style="color:#e1e4e8;">    var silce = Array.prototype.slice;</span></span>
+<span class="line"><span style="color:#e1e4e8;">    return function() {</span></span>
+<span class="line"><span style="color:#e1e4e8;">      if(arguments.length &lt; length) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">        var combined = [fn].concat(slice.call(arguments));</span></span>
+<span class="line"><span style="color:#e1e4e8;">        return curry(sub_curry.apply(this, combined), length - arguments.length);</span></span>
+<span class="line"><span style="color:#e1e4e8;">      } else {</span></span>
+<span class="line"><span style="color:#e1e4e8;">        return fn.apply(this, arguments)</span></span>
+<span class="line"><span style="color:#e1e4e8;">      }</span></span>
+<span class="line"><span style="color:#e1e4e8;">    }</span></span>
+<span class="line"><span style="color:#e1e4e8;">  }</span></span></code></pre></div><h2 id="惰性函数" tabindex="-1">惰性函数 <a class="header-anchor" href="#惰性函数" aria-label="Permalink to &quot;惰性函数&quot;">​</a></h2><ul><li>惰性函数就是解决每次都要进行判断的这个问题，解决原理很简单，重写函数。<div class="language-"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki github-dark"><code><span class="line"><span style="color:#e1e4e8;">// eg:在dom事件添加中，为了兼容现代浏览器和IE浏览器，我们需要对浏览器环境进行一次判断。</span></span>
+<span class="line"><span style="color:#e1e4e8;">  function addEvent (type, el, fn) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">      if (window.addEventListener) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">          addEvent = function (type, el, fn) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">              el.addEventListener(type, fn, false);</span></span>
+<span class="line"><span style="color:#e1e4e8;">          }</span></span>
+<span class="line"><span style="color:#e1e4e8;">      }</span></span>
+<span class="line"><span style="color:#e1e4e8;">      else if(window.attachEvent){</span></span>
+<span class="line"><span style="color:#e1e4e8;">          addEvent = function (type, el, fn) {</span></span>
+<span class="line"><span style="color:#e1e4e8;">              el.attachEvent(&#39;on&#39; + type, fn);</span></span>
+<span class="line"><span style="color:#e1e4e8;">          }</span></span>
+<span class="line"><span style="color:#e1e4e8;">      }</span></span>
+<span class="line"><span style="color:#e1e4e8;">  }</span></span></code></pre></div></li></ul></li></ul>`,2)]))}const g=n(p,[["render",t]]);export{u as __pageData,g as default};
