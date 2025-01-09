89f888649394abdbc8f64b97f047bd5ad013d622
tunaizi <tunaizi@users.noreply.github.com>
2025-01-09 09:45:38 +0000

    deploy: 7b2bee331621d01e04990b7e725d996ab0384cce

---
+++ assets/start-docs_create-blog.md.65ffc3c8.js
@@ -0,0 +1,45 @@
+import{_ as n,r as a,i as l,a as o}from"./chunks/framework.bb8f9838.js";const d=JSON.parse('{"title":"快速上手vitePress","description":"","frontmatter":{"editLink":false,"title":"快速上手vitePress","footer":false},"headers":[],"relativePath":"start-docs/create-blog.md","filePath":"start-docs/create-blog.md"}'),p={name:"start-docs/create-blog.md"};function e(t,s,c,E,r,i){return a(),l("div",null,s[0]||(s[0]=[o(`<h1 id="markdown-语法示例文档" tabindex="-1">Markdown 语法示例文档 <a class="header-anchor" href="#markdown-语法示例文档" aria-label="Permalink to &quot;Markdown 语法示例文档&quot;">​</a></h1><div class="language-markdown"><button title="Copy Code" class="copy"></button><span class="lang">markdown</span><pre class="shiki github-dark"><code><span class="line"><span style="color:#79B8FF;font-weight:bold;">## 目录</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">标题</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#标题</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">段落和换行</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#段落和换行</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">强调文本</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#强调文本</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">列表</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#列表</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">链接</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#链接</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">图片</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#图片</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">代码块</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#代码块</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">引用</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#引用</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">水平线</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#水平线</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">表格</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#表格</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"><span style="color:#FFAB70;">-</span><span style="color:#E1E4E8;"> [</span><span style="color:#DBEDFF;text-decoration:underline;">转义字符</span><span style="color:#E1E4E8;">](</span><span style="color:#E1E4E8;text-decoration:underline;">#转义字符</span><span style="color:#E1E4E8;">)</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">## 标题</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">使用 </span><span style="color:#79B8FF;">\`#\`</span><span style="color:#E1E4E8;"> 来定义标题，不同数量的 </span><span style="color:#79B8FF;">\`#\`</span><span style="color:#E1E4E8;"> 表示不同级别的标题。</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;"># 一级标题</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">## 二级标题</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">### 三级标题</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">#### 四级标题</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">##### 五级标题</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">###### 六级标题</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">## 表格</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">| Tables        |      Are      |  Cool |  Cool |  Cool |  Cool |</span></span>
+<span class="line"><span style="color:#E1E4E8;">| ------------- | :-----------: | ----: | ----: | ----: | ----: |</span></span>
+<span class="line"><span style="color:#E1E4E8;">| col 3 is      | right-aligned | $1600 | $1600 | $1600 | $1600 |</span></span>
+<span class="line"><span style="color:#E1E4E8;">| col 2 is      |   centered    |   $12 |   $12 |   $12 |   $12 |</span></span>
+<span class="line"><span style="color:#E1E4E8;">| zebra stripes |   are neat    |    $1 |    $1 |    $1 |    $1 |</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#79B8FF;font-weight:bold;">## 代码段落</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#E1E4E8;">\`\`\`html</span></span>
+<span class="line"><span style="color:#E1E4E8;">&lt;</span><span style="color:#85E89D;">div</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">  &lt;</span><span style="color:#85E89D;">span</span><span style="color:#E1E4E8;"> </span><span style="color:#B392F0;">v-for</span><span style="color:#E1E4E8;">=</span><span style="color:#9ECBFF;">&quot;i in 3&quot;</span><span style="color:#E1E4E8;">&gt;{{ i }}&lt;/</span><span style="color:#85E89D;">span</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">&lt;/</span><span style="color:#85E89D;">div</span><span style="color:#E1E4E8;">&gt;</span></span>
+<span class="line"><span style="color:#E1E4E8;">\`\`\`</span></span></code></pre></div><p>🎉 💯 🎉 💯 🎉 💯 🎉 💯 🎉 💯 🎉 💯</p><p>//--所有的表情 markdown-it-emoji lib/data/full.mjs</p><div class="info custom-block"><p class="custom-block-title">INFO</p><p>This is an info box.</p></div><div class="tip custom-block"><p class="custom-block-title">TIP</p><p>This is a tip.</p></div><div class="warning custom-block"><p class="custom-block-title">WARNING</p><p>This is a warning.</p></div><div class="danger custom-block"><p class="custom-block-title">DANGER</p><p>This is a dangerous warning.</p></div><details class="details custom-block"><summary>Details</summary><p>This is a details block.</p></details>`,9)]))}const F=n(p,[["render",e]]);export{d as __pageData,F as default};
